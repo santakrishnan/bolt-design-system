aabf0d053aa8ae886cd0cfd4ed51af6d9d520dd6
Santhana Krishnan <santa.krishnan@borngroup.com>
2026-01-28 16:18:35 -0500

    chore: remove unused proxy.ts file

--- proxy.ts
+++
@@ -1,6 +0,0 @@
-import { type NextRequest, NextResponse } from "next/server";
-
-export function proxy(_request: NextRequest) {
-  // Analytics removed - proxy now just passes through requests
-  return NextResponse.next();
-}
